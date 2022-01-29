0d3c105d79241c254859d9f3eebc6c57da44d563
Tom Hackshaw <42124348+et0and@users.noreply.github.com>
2022-01-30 00:40:33 +1300

    Updated footer

--- components/Footer.js
+++ components/Footer.js
@@ -7,14 +7,14 @@ export default function Footer() {
   return (
     <footer className="mt-20 layout px-4 md:px-8 flex justify-between items-center pt-8 md:pt-10 lg:pt-12 pb-10 md:pb-12 lg:pb-24">
         <Link href="/">
-          <a className="type-ui">The Urbit Guidebook</a>
+          <a className="type-ui">Exhibitions</a>
         </Link>
 
         <nav className="flex">
         {/* <Link href="/faq">
           <a className="type-ui mr-4">FAQ</a>
         </Link> */}
-        <a className="type-ui flex" href="https://urbit.org" target="_blank">urbit.org <div className="font-sans w-8 h-8 rounded-full bg-wall-600 dark:bg-antiwall-600 ml-2 text-wall-100 dark:text-antiwall-100 flex items-center justify-center"><p className="leading-none pt-0.5">{'↗'}</p></div></a>
+        <a className="type-ui flex" href="https://tewahi.com" target="_blank">Te Wāhi Auaha <div className="font-sans w-8 h-8 rounded-full bg-wall-600 dark:bg-antiwall-600 ml-2 text-wall-100 dark:text-antiwall-100 flex items-center justify-center"><p className="leading-none pt-0.5">{'↗'}</p></div></a>
         </nav>
     </footer>
   );
